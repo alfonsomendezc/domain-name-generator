85f016826284508fbe3c171dd5ccbe27e6a8083a
alfonsomendezc <alfonsomendez2209@gmail.com>
2022-04-06 21:58:07 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -2,42 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-function generateExcuse() {
-  let who = [
-    "The dog",
-    "My grandma",
-    "His turtle",
-    "My bird",
-    "My tarantula",
-    "Cristiano Ronaldo"
-  ];
-  let action = ["ate", "peed on", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car", "my phone"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was sleeping",
-    "while I was in the bathroom"
-  ];
+let pronoun = ["the", "our"];
+let adj = ["incredible", "amazing", "stinky", "surprising", "suspicious"];
+let noun = ["shop", "tarantula", "jean", "messi"];
+let extension = [".net", ".org", ".com", ".io"];
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
-
-  return (
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex]
-  );
+function selRandom(arr) {
+  let random = ~~(Math.random() * arr.length);
+  return arr[random];
 }
 
 window.onload = function() {
-  document.querySelector("#excuse").innerHTML = generateExcuse();
+  let result = `${selRandom(pronoun)}${selRandom(adj)}${selRandom(
+    noun
+  )}${selRandom(extension)}`;
+  let element = document.getElementById("domain-name");
+  element.innerHTML = result;
 };
