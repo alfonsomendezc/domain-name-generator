97b6a60e02025cb4e91b8ee7e0828b09516f7441
alfonsomendezc <alfonsomendez2209@gmail.com>
2022-04-07 22:14:41 +0000

    cambios v2

--- src/app.js
+++ src/app.js
@@ -7,15 +7,14 @@ let adj = ["incredible", "amazing", "stinky", "surprising", "suspicious"];
 let noun = ["shop", "tarantula", "jean", "messi"];
 let extension = [".net", ".org", ".com", ".io"];
 
-function selRandom(arr) {
-  let random = ~~(Math.random() * arr.length);
-  return arr[random];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        let result = `${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`;
+        let element = document.getElementById("domain-name");
+        element.innerHTML = result;
+      }
+    }
+  }
 }
-
-window.onload = function() {
-  let result = `${selRandom(pronoun)}${selRandom(adj)}${selRandom(
-    noun
-  )}${selRandom(extension)}`;
-  let element = document.getElementById("domain-name");
-  element.innerHTML = result;
-};
